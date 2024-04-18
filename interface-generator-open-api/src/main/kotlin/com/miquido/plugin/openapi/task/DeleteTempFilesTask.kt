eeb17f8df4a998bfb464986093cbc4e73ccaaca5
Przemysław Książek <przemyslaw.ksiazek@b-connected.io>
2024-04-18 09:05:53 +0200

    Remove unnecessary files after generate class

--- interface-generator-open-api/src/main/kotlin/com/miquido/plugin/openapi/task/DeleteTempFilesTask.kt
+++ interface-generator-open-api/src/main/kotlin/com/miquido/plugin/openapi/task/DeleteTempFilesTask.kt
@@ -6,5 +6,7 @@ import org.gradle.api.tasks.Delete
 fun deleteTempFilesTask(): Delete.() -> Unit = {
     Constant.run {
         delete(project.layout.projectDirectory.dir(tempDirectoryName))
+        delete(project.layout.projectDirectory.dir(specificationDir))
+        delete(project.layout.projectDirectory.dir(configurationDir))
     }
 }
